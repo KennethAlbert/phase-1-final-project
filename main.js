334665847b14a83d9344f4e16712da4a45a45bd1
Kenneth Albert <gichuka@gmail.com>
2022-06-26 12:56:19 +0300

    setting up global variables in main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+//setting global variables
+
+let productName=document.querySelector('form #productName');
+let productPrice= document.querySelector('form #price')
+let productimage=  document.querySelector('form #image')
+let productCategory=document.querySelector('form #categery')
+let productDescription=document.querySelector('form #message')
+let form=document.querySelector('form')
+
+let cards=document.querySelector('#cards');
+let price=document.querySelector('.card #price');
+let catergory=document.querySelector('.card #catergory');
+let title=document.querySelector('.card h1');
+let cardImg=document.querySelector('.card img');
+let description=document.querySelector('.card p');
+let cartValue=document.querySelector('#cart-btn i')
+let addCartbtn=document.querySelector('#addCart');
+let subTotal=document.querySelector('.total p')
+
